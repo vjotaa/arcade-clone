84c98dc5c0af7e6753957789da62d9711e426d59
vjotaa <vjotaa@gmail.com>
2019-09-19 20:59:19 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'arcade-clone';
+  public imageSize = 100;
+  private lastScrollTop = 0;
+
+  window.addEventListener('scroll', function() {
+    console.log('scrolling');
+});
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+    const value = window.pageYOffset;
+    console.log(value);
+    const direction = this._getScrollDirection(value);
+    if (direction === 'up' && this.imageSize < 100) {
+      this.imageSize = this.imageSize + 1;
+      return;
+    }
+
+    if (direction === 'down' && value > 100) {
+      this.imageSize = this.imageSize - 1;
+    }
+  }
+
+  private _getScrollDirection(value: number): string {
+    let dir = '';
+    if (value > this.lastScrollTop) {
+      dir = 'down';
+    } else {
+      dir = 'up';
+    }
+    this.lastScrollTop = value;
+    return dir;
+  }
 }
